a8c0a98158bd7453fe94c39971d088442fb01c45
kelechi chukwu <leochukwu48@gmail.com>
2024-03-29 11:31:12 +0100

    initial commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product,Profile,Cateogry,Customer
 # Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(Profile)
+admin.site.register(Cateogry)
+admin.site.register(Customer)
