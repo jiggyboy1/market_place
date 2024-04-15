c55552543497221850ef53ed7028335ba26ddd08
kelechi chukwu <leochukwu48@gmail.com>
2024-04-15 11:57:41 +0100

    initial commit

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('product/<str:pk>',views.review, name='product')
+    path('product/<str:pk>',views.review, name='product'),
+    path('category/',views.category, name='category'),
+    path('category/<str:foo>/',views.category_page, name='category_page'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -14,3 +14,15 @@ def review(request,pk):
     room = Product.objects.get(id=pk)
     context = {'room':room}
     return render(request,'product.html',context)
+
+def category(request):
+    product = Product.objects.all()
+    cateogry = Cateogry.objects.all()
+    context = {'product':product,'cateogry':cateogry}
+    return render(request,'category.html',context)
+
+def category_page(request,foo):
+    cateogry = Cateogry.objects.get(name=foo)
+    product = Product.objects.filter(cateogry=cateogry)
+    context = {'product':product,'cateogry':cateogry}
+    return render(request,'category_page.html',context)
