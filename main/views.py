522abed8b1d91da5d4faf1e9eacfa335c7e6ef89
kelechi chukwu <leochukwu48@gmail.com>
2024-04-19 12:58:51 +0100

    initial commit

--- main/views.py
+++ main/views.py
@@ -38,9 +38,19 @@ def login_user(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    
+    return render(request,'login.html')
+
 def logout_user(request):
     logout(request)
     return redirect('home')
+
 
 def register(request):
     form = Register_form()
