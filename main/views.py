e19266f762fba70df23df57a6d18f5d469030d10
kelechi chukwu <leochukwu48@gmail.com>
2024-04-30 12:07:07 +0100

    initial commit

--- main/views.py
+++ main/views.py
@@ -59,6 +59,7 @@ def login_user(request):
         user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
+            messages.success(request,'You have logged in')
             return redirect('home')
         else:
             return redirect('login')
